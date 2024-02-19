e1cdb497ccabc39f15d40635f1b780c050918962
hichamelg110 <elisejuneaileen@gmail.com>
2024-02-19 12:06:09 -0800

    task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,33 +14,21 @@ tmp = *a;
 *b = tmp;
 }
 
-/**
- * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers to sort.
- * @size: The size of the array.
- *
- * Description: Prints the array after each swap.
- */
 void bubble_sort(int *array, size_t size)
 {
-size_t i, len = size;
-bool c = false;
-
 if (array == NULL || size < 2)
 return;
 
-while (c == false)
-{
-c = true;
-for (i = 0; i < len - 1; i++)
-{
-if (array[i] > array[i + 1])
-{
+size_t i, len;
+bool swap;
+
+for (len = size; len > 1 && (swap = false); len--) {
+for (i = 0; i < len - 1; i++) {
+if (array[i] > array[i + 1]) {
 swap_ints(array + i, array + i + 1);
 print_array(array, size);
-c = false;
+swap = true;
 }
 }
-len--;
 }
 }
