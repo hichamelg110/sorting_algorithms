34ac9bc3dcc9f5c18a0ad1d32e134004f68bae0a
hichamelg110 <elisejuneaileen@gmail.com>
2024-02-20 10:06:27 -0800

    task

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,19 +6,23 @@
  * @n1: A pointer to the first node to swap.
  * @n2: The second node to swap.
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2)
 {
-(*n1)->next = n2->next;
-if (n2->next != NULL)
-n2->next->prev = *n1;
-n2->prev = (*n1)->prev;
-n2->next = *n1;
-if ((*n1)->prev != NULL)
-(*n1)->prev->next = n2;
+(*node1)->next = node2->next;
+if (node2->next != NULL)
+node2->next->prev = *node1;
+
+node2->prev = (*node1)->prev;
+node2->next = *node1;
+
+if ((*node1)->prev != NULL)
+(*node1)->prev->next = node2;
+
 else
-*h = n2;
-(*n1)->prev = n2;
-*n1 = n2->prev;
+*head = node2;
+
+(*node1)->prev = node2;
+*node1 = node2->prev;
 }
 
 /**
