0638936a5973238bd214a7f099abb7d0962d8fb4
hichamelg110 <elisejuneaileen@gmail.com>
2024-02-20 09:44:57 -0800

    task

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,47 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swap two nodes in a listint_t doubly linked list
- * @head: Pointer to the head of the doubly linked list
- * @node1: Pointer to the first node to swap
- * @node2: The second node to swap
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
  */
-void swap_nodes(listint_t **head, listint_t **node1, listint_t *node2)
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-(*node1)->next = node2->next;
-if (node2->next != NULL)
-node2->next->prev = *node1;
-        
+(*n1)->next = n2->next;
+if (n2->next != NULL)
+n2->next->prev = *n1;
+n2->prev = (*n1)->prev;
+n2->next = *n1;
+if ((*n1)->prev != NULL)
+(*n1)->prev->next = n2;
+else
+*h = n2;
+(*n1)->prev = n2;
+*n1 = n2->prev;
+}
+
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
- * @list: Pointer to the head of a doubly linked list of integers.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
  */
- void insertion_sort_list(listint_t **list)
+void insertion_sort_list(listint_t **list)
 {
-listint_t *c, *v;
+listint_t *iter, *insert, *tmp;
 
 if (list == NULL || *list == NULL || (*list)->next == NULL)
 return;
 
-for (c = (*list)->next; c != NULL; c = c->next)
+for (iter = (*list)->next; iter != NULL; iter = tmp)
 {
-for (v = c; v != NULL && v->prev != NULL; v = v->prev)
+tmp = iter->next;
+insert = iter->prev;
+while (insert != NULL && iter->n < insert->n)
 {
-if (v->n < v->prev->n)
-{
-swap(v->prev, v);
-if (!v->prev)
-*list = v;
+swap_nodes(list, &insert, iter);
 print_list((const listint_t *)*list);
 }
-else
-break;
 }
 }
-}
-
