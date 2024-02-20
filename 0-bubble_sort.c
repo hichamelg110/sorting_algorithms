9ad026d9bd29ea674280a6a5beeba08cef1287d7
hichamelg110 <elisejuneaileen@gmail.com>
2024-02-20 02:20:51 -0800

    task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,9 +15,9 @@ tmp = *a;
 }
 
 /**
- * bubble_sort - Sort an array of integers in ascending order
- * @array: The array of integers
- * @size: The size of the array
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
  *
  * Description: Prints the array after each swap.
  */
@@ -36,7 +36,7 @@ for (i = 0; i < len - 1; i++)
 {
 if (array[i] > array[i + 1])
 {
-swap(array + i, array + i + 1);
+swap_ints(array + i, array + i + 1);
 print_array(array, size);
 c = 1;
 }
@@ -44,4 +44,3 @@ c = 1;
 len--;
 }
 }
-
